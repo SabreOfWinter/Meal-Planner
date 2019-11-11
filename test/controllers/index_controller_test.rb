7b90d1d98ca4997cf83aef7da8a1b315c71f1a17
bs00645 <bs00645@surrey.ac.uk>
2019-11-11 19:43:53 +0000

    Testing added for home page

--- test/controllers/index_controller_test.rb
+++ test/controllers/index_controller_test.rb
@@ -2,8 +2,12 @@ require 'test_helper'
 
 class IndexControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get index_home_url
+    get root_url
     assert_response :success
+
+    assert_select 'title', 'Meal Planner'
+    assert_select 'h1', 'Home'
+    assert_select 'p', 'Welcome Home!'
   end
 
   test "should get contact" do
