ba02133dd2a99da81107296c40a534c30e351171
bs00645 <bs00645@surrey.ac.uk>
2019-11-11 22:13:51 +0000

    Contact form functionality set up

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -4,4 +4,20 @@ class IndexController < ApplicationController
 
   def contact
   end
+
+  def request_contact
+    name = params[:name]
+    email = params[:email]
+    telephone = params[:telephone]
+    message = params[:message]
+
+    if email.blank?
+      flash[:alert] = I18n.t('index.request_contact.no_email')
+    else
+      #send email
+      flash[:notice] = I18n.t('index.request_contact.email_sent')
+    end
+
+    redirect_to root_path
+  end
 end
