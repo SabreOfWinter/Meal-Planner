6d0c3357b04539d912e9a80c274db2d6886a9dfa
bs00645 <bs00645@surrey.ac.uk>
2019-11-22 19:39:24 +0000

    Meal scaffold

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,9 @@
 class IndexController < ApplicationController
+
+  before_action :authenticate_user!
+
   def home
+
   end
 
   def contact
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  resources :meals
   devise_for :users
+
+  devise_scope :user do
+    get '/sign_in', to: 'devise/sessions#new'
+    get '/sign_out', to: 'devise/sessions#destroy'
+  end
+
   root 'index#home'
   get 'index/contact', to: 'index#contact'
   post 'request_contact', to: 'index#request_contact'
